da9bf372accc237da321e296c2a9b942edc5215f
Larry <larry@ike.rau>
2018-09-01 11:56:27 -0400

    first tool; add go cmd

---
+++ go.rb
@@ -0,0 +1,112 @@
+class Go < Formula
+  desc "Open source programming language to build simple/reliable/efficient software"
+  homepage "https://golang.org"
+
+  stable do
+    url "https://dl.google.com/go/go1.11.src.tar.gz"
+    mirror "https://fossies.org/linux/misc/go1.11.src.tar.gz"
+    sha256 "afc1e12f5fe49a471e3aae7d906c73e9d5b1fdd36d52d72652dde8f6250152fb"
+
+    go_version = version.to_s.split(".")[0..1].join(".")
+    resource "gotools" do
+      url "https://go.googlesource.com/tools.git",
+          :branch => "release-branch.go#{go_version}"
+    end
+  end
+
+  bottle do
+    sha256 "46c54a3db461ce4ab9450991a84859c5d3e7bfc908ac1e99cc5b6dff0dea0940" => :mojave
+    sha256 "d2dfd9846bc03498dd69c01eaccc0a3018a66d00864d7433d70f07645aac6a40" => :high_sierra
+    sha256 "658842f7da7e1e922b4242f14df5dcd7c7b42055d36d3b04997be4b2f00a6c75" => :sierra
+    sha256 "ebce66f5a06cdb481241bf1952792b1569a9fee0c5ea5747fc5a13d4f1e87cbc" => :el_capitan
+  end
+
+  head do
+    url "https://go.googlesource.com/go.git"
+
+    resource "gotools" do
+      url "https://go.googlesource.com/tools.git"
+    end
+  end
+
+  option "without-cgo", "Build without cgo (also disables race detector)"
+  option "without-race", "Build without race detector"
+
+  depends_on :macos => :yosemite
+
+  # Don't update this unless this version cannot bootstrap the new version.
+  resource "gobootstrap" do
+    url "https://storage.googleapis.com/golang/go1.7.darwin-amd64.tar.gz"
+    version "1.7"
+    sha256 "51d905e0b43b3d0ed41aaf23e19001ab4bc3f96c3ca134b48f7892485fc52961"
+  end
+
+  def install
+    (buildpath/"gobootstrap").install resource("gobootstrap")
+    ENV["GOROOT_BOOTSTRAP"] = buildpath/"gobootstrap"
+
+    cd "src" do
+      ENV["GOROOT_FINAL"] = libexec
+      ENV["GOOS"]         = "darwin"
+      ENV["CGO_ENABLED"]  = "0" if build.without?("cgo")
+      system "./make.bash", "--no-clean"
+    end
+
+    (buildpath/"pkg/obj").rmtree
+    rm_rf "gobootstrap" # Bootstrap not required beyond compile.
+    libexec.install Dir["*"]
+    bin.install_symlink Dir[libexec/"bin/go*"]
+
+    # Race detector only supported on amd64 platforms.
+    # https://golang.org/doc/articles/race_detector.html
+    if build.with?("cgo") && build.with?("race") && MacOS.prefer_64_bit?
+      system bin/"go", "install", "-race", "std"
+    end
+
+    # Build and install godoc
+    ENV.prepend_path "PATH", bin
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/golang.org/x/tools").install resource("gotools")
+    cd "src/golang.org/x/tools/cmd/godoc/" do
+      system "go", "build"
+      (libexec/"bin").install "godoc"
+    end
+    bin.install_symlink libexec/"bin/godoc"
+  end
+
+  def caveats; <<~EOS
+    A valid GOPATH is required to use the `go get` command.
+    If $GOPATH is not specified, $HOME/go will be used by default:
+      https://golang.org/doc/code.html#GOPATH
+
+    You may wish to add the GOROOT-based install location to your PATH:
+      export PATH=$PATH:#{opt_libexec}/bin
+  EOS
+  end
+
+  test do
+    (testpath/"hello.go").write <<~EOS
+      package main
+
+      import "fmt"
+
+      func main() {
+          fmt.Println("Hello World")
+      }
+    EOS
+    # Run go fmt check for no errors then run the program.
+    # This is a a bare minimum of go working as it uses fmt, build, and run.
+    system bin/"go", "fmt", "hello.go"
+    assert_equal "Hello World\n", shell_output("#{bin}/go run hello.go")
+
+    # godoc was installed
+    assert_predicate libexec/"bin/godoc", :exist?
+    assert_predicate libexec/"bin/godoc", :executable?
+
+    if build.with? "cgo"
+      ENV["GOOS"] = "freebsd"
+      system bin/"go", "build", "hello.go"
+    end
+  end
+end
+
